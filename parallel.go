7e5693a72824d1004a3fa565a9a7c1c2fe610f31
Craig Weber <weberc2@gmail.com>
2016-02-06 12:26:58 -0600

    Improved parallel.go subslice() implementation; improved formatting

--- parallel.go
+++ parallel.go
@@ -31,59 +31,30 @@ func validateRows(rows [][]string, colSize int) {
 		}
 		for colID, cell := range row {
 			if _, err := strconv.Atoi(cell); err != nil {
-				fmt.Fprintf(os.Stderr, "Err at (%d, %d): %v\n", colID, rowID, err)
+				msg := "Err at (%d, %d): %v\n"
+				fmt.Fprintf(os.Stderr, msg, colID, rowID, err)
 			}
 		}
 	}
-}
-
-func timeit(f func()) time.Duration {
-	start := time.Now()
-	f()
-	return time.Now().Sub(start)
 }
 
 // Divides `slice` into `n` subslices such that the elements are distributed
 // as evenly as possible. In other words, if there are 10 elements in `slice`,
 // and `n` is 3, there will be one subslice with 4 elements and the others will
 // have only 3.
-func subslice(slice [][]string, n int) [][][]string {
-	blockSize := len(slice) / n
-	remainder := len(slice) % n
-	subslices := make([][][]string, n)
-
-	// It's not enough just to make n subslices of len(slice)/n elements,
-	// because integer division means that n*len(slice)/n != len(slice). For
-	// example, if slice contains 10 elements and n is 4, then we'd be creating
-	// 4 subslices with 10/4=2 elements each, and 2*4 != 10. To work around
-	// this, we'll initially compute the remainder (10%4 = 2) which tells us
-	// how many subslices will need to have len(slice)/n + 1 elements
-	// (10/4+1=3) elements, while the rest will have only len(slice)/n
-	// elements.
-	//
-	// The first for loop creates those slightly-larger subslices, while the
-	// second creates the slightly-smaller subslices. In this way, the
-	// difference between the largest and smallest subslices will be at most 1
-	// element.
-	for i := 0; i < remainder; i++ {
-		start := i * (blockSize + 1)
-		end := (i + 1) * (blockSize + 1)
-		subslices[i] = slice[start:end]
+func subslice(s [][]string, n int) (ret [][][]string) {
+	for ; n > 0; n-- {
+		s, ret = s[len(s)/n:], append(ret, s[:len(s)/n])
 	}
-
-	for i := remainder; i < n; i++ {
-		start := i*blockSize + remainder
-		end := (i+1)*blockSize + remainder
-		subslices[i] = slice[start:end]
-	}
-
-	return subslices
+	return ret
 }
 
 func validateParallel(rows [][]string, coreCount int) {
 	wg := sync.WaitGroup{}
-	wg.Add(coreCount)
+	wg.Add(coreCount) // Add `coreCount` goroutines to the WaitGroup
 
+	// divide `rows` into `coreCount` blocks of rows, and then dispatch a
+	// goroutine to process each block.
 	for _, block := range subslice(rows, coreCount) {
 		// Create a new variable exclusively for the goroutine that corresponds
 		// to this loop iteration. All goroutines can't share one variable,
@@ -95,11 +66,17 @@ func validateParallel(rows [][]string, coreCount int) {
 
 		go func() {
 			validateRows(block, len(rows[0]))
-			wg.Done()
+			wg.Done() // signal that this goroutine has finished execution
 		}()
 	}
 
-	wg.Wait()
+	wg.Wait() // block until `wg.Done()` has been called `coreCount` times
+}
+
+func timeit(f func()) time.Duration {
+	start := time.Now()
+	f()
+	return time.Now().Sub(start)
 }
 
 func main() {
